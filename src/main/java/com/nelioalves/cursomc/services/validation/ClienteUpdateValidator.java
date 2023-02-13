ddd6024fabfe3f95eef2b43c5ffad3c767af42d4
MateusCassio10 <mateuscassio07@gmail.com>
2023-02-13 16:21:01 -0300

    Validação customizada: email não repetido na atualização de Cliente

--- src/main/java/com/nelioalves/cursomc/services/validation/ClienteUpdateValidator.java
+++ src/main/java/com/nelioalves/cursomc/services/validation/ClienteUpdateValidator.java
@@ -30,6 +30,7 @@ public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate
     @Override
     public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
 
+        @SuppressWarnings("unchecked")
         Map<String, String> map = (Map<String, String>) httpServletRequest.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
         Integer uriId = Integer.parseInt(map.get("id"));
 
