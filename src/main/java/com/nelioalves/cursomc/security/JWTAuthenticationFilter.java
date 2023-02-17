cb4bcfdfa796e79fbcdd286799f696f34dde33e2
MateusCassio10 <mateuscassio07@gmail.com>
2023-02-17 13:14:40 -0300

    Implementando autorização

--- src/main/java/com/nelioalves/cursomc/security/JWTAuthenticationFilter.java
+++ src/main/java/com/nelioalves/cursomc/security/JWTAuthenticationFilter.java
@@ -17,13 +17,14 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 
+
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     private final AuthenticationManager authenticationManager;
     private final JWTUtil jwtUtil;
 
     public JWTAuthenticationFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil) {
-        //setAuthenticationFailureHandler(new JWTAuthenticationFailureHandler());
+        setAuthenticationFailureHandler(new JWTAuthenticationFailureHandler());
         this.authenticationManager = authenticationManager;
         this.jwtUtil = jwtUtil;
     }
@@ -57,25 +58,23 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     }
 
 
+    private class JWTAuthenticationFailureHandler implements AuthenticationFailureHandler {
 
+        @Override
+        public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
+                throws IOException, ServletException {
+            response.setStatus(401);
+            response.setContentType("application/json");
+            response.getWriter().append(json());
+        }
 
-//    private class JWTAuthenticationFailureHandler implements AuthenticationFailureHandler {
-//
-//        @Override
-//        public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
-//                throws IOException, ServletException {
-//            response.setStatus(401);
-//            response.setContentType("application/json");
-//            response.getWriter().append(json());
-//        }
-//
-//        private String json() {
-//            long date = new Date().getTime();
-//            return "{\"timestamp\": " + date + ", "
-//                    + "\"status\": 401, "
-//                    + "\"error\": \"Não autorizado\", "
-//                    + "\"message\": \"Email ou senha inválidos\", "
-//                    + "\"path\": \"/login\"}";
-//        }
-//    }
+        private String json() {
+            long date = new Date().getTime();
+            return "{\"timestamp\": " + date + ", "
+                    + "\"status\": 401, "
+                    + "\"error\": \"Não autorizado\", "
+                    + "\"message\": \"Email ou senha inválidos\", "
+                    + "\"path\": \"/login\"}";
+        }
+    }
 }
